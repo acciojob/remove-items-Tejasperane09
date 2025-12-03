168f86993c3fb8318e09011df377f3809c3bbe6e
Tejasperane09 <89592090+Tejasperane09@users.noreply.github.com>
2025-12-03 11:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("removeBtn").onclick = function () {
+  const select = document.getElementById("colorSelect");
+
+ 
+  if (select.selectedIndex === -1) return;
+
+
+  select.remove(select.selectedIndex);
+};
